801c947866def8f057816c9e06151d51a52e6627
br1anp4rk <br1anp4rk718@gmail.com>
2015-10-11 14:03:49 -0700

    items controller and items populated

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,28 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  # GET /items
+  # GET /items.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /items/1
+  # GET /items/1.json
+  def show
+  end
+
+  # GET /items/new
+  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(:name, :price, :image, :description, :asin)
+    end
+end
---
+++ app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :name, :price, :image, :description, :asin, :created_at, :updated_at
