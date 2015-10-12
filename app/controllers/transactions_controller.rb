28ca38ef7c0e8f57d7bc2a28aa884179141f4fa5
Ana Nelson <anaclairnelson@gmail.com>
2015-10-12 12:11:14 -0700

    Add check_cart method to transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,14 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user! # Check that user is signed in
+  before_action :check_cart! # Check that cart has items
+
   def new
+  end
+
+  private
+  def check_cart!
+    if current_user.get_cart_items.blank?
+      redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
+    end
   end
 end
