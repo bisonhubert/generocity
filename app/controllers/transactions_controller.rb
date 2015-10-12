0adb8a3a1187d22925eed5dcfe7d7f8ea3e2509e
Ana Nelson <anaclairnelson@gmail.com>
2015-10-12 12:21:09 -0700

    Update transactions#create to submit a Braintree Transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,19 +1,35 @@
 class TransactionsController < ApplicationController
-  before_action :authenticate_user! # Check that user is signed in
-  before_action :check_cart! # Check that cart has items
+  before_action :authenticate_user!
+  before_action :check_cart
 
   def new
     gon.client_token = generate_client_token
   end
 
+  def create
+    @result = Braintree::Transaction.sale(
+              amount: current_user.cart_total_price,
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success? # If validations do not deny the transaction from being authorized on Braintree
+      current_user.purchase_cart_items!
+      redirect_to root_url, notice: "Your donation was successful. Thank you for being an amazing person!"
+    else
+      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      gon.client_token = generate_client_token
+      render :new
+    end
+  end
+
   private
+
   def generate_client_token
     Braintree::ClientToken.generate
   end
 
-  def check_cart!
+  def check_cart
     if current_user.get_cart_items.blank?
       redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
     end
   end
+
 end
