6e6e1672fd8e526506a6e65606a66346c6231972
Ana Nelson <anaclairnelson@gmail.com>
2015-10-11 19:13:24 -0700

    Add carts controller, declare cart as singular resource, define routes for adding/removing items from cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'carts/show'
+
   resources :items, only: [:show, :index]
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  # devise_for :users
+
+  resource :cart, only: [:show] do
+    put 'add/:item_id', to: 'carts#add', as: :add_to
+    put 'remove/:item_id', to: 'carts#remove', as: :remove_from
+  end
+
   get 'welcome/index'
   root 'items#index'
 
