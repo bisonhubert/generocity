d627ed34a4ebe43e51111495afb87882e31a7423
davidbison <davidbisonhubert@gmail.com>
2015-10-15 10:13:54 -0700

    Add auth token filter and image size edit to show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   before_action :authenticate_user!
 
   def show
