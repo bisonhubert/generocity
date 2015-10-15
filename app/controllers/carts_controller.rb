165cf92ed96af261277212b3d3ba140164005ccc
br1anp4rk <br1anp4rk718@gmail.com>
2015-10-15 10:56:29 -0700

    fixed donation button route

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
-  before_action :authenticate_user!
+   skip_before_filter :verify_authenticity_token
+   before_action :authenticate_user!
 
   def show
     cart_ids = $redis.hkeys current_user_cart
