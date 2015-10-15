dcc73e37721674d5e8e36d69cf9a939fcecc467d
Ana Nelson <anaclairnelson@gmail.com>
2015-10-15 09:38:34 -0700

    Add header with logo, styling not finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+# Devise helpers for rendering Devise User partials
+
+def resource_name
+  :user
+end
+
+def resource
+  @resource ||= User.new
+end
+
+def devise_mapping
+  @devise_mapping ||= Devise.mappings[:user]
+end
+
+def resource_class
+  devise_mapping.to
+end
 end
